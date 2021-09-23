2f89cabd9a9978503b005b6f6c8c8652a01afe4a
Morse <enmorse48@gmail.com>
2021-09-23 12:06:15 -0700

    Initial commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+# Store the names of a few of your friends in a list
+# named "names". Print each person's name by
+# accessing each element in the list, one at a time.
+
+names = ["john", "nate", "vincent"]
+
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
